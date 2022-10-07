bd50a802315533195efa4e38bff9b27769fcb837
João Carvalho <joaove>
2022-10-07 19:42:35 +0100

    push

--- basedailym8/models.py
+++ basedailym8/models.py
@@ -1,3 +1,18 @@
+from asyncio.windows_events import NULL
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    #host =
+    #topic =
+    Username = models.TextField(_MAX_LENGTH=200)(NULL=False)
+    UserBdate = models.DateTimeField(NULL=False)
+    UserGender = models.CharField(NULL=False)
+    UserEmail = models.TextField(_MAX_LENGTH=200)(NULL=False)
+    UserMatricula = models.CharField(_MAX_LENGTH=8)(NULL=False)
+    UserPassword = models.TextField(_MAX_LENGTH=25)(NULL=False)
+    updated = models.DateTimeField(auto_now=True)
+    
