5f6c16e2a6cc969daf4e5d16487e572df884e4e6
João Carvalho <joaove>
2022-10-18 17:14:14 +0100

    dwyugw

--- basedailym8/models.py
+++ basedailym8/models.py
@@ -1,8 +1,8 @@
-from asyncio.windows_events import NULL
 from email.policy import default
 from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
+
 
 
 # Create your models here.
