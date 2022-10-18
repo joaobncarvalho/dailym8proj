0d8745103e0a24fe335493eb28a736fb2fb1bdf7
João Carvalho <joaove>
2022-10-18 16:49:14 +0100

    2

--- basedailym8/models.py
+++ basedailym8/models.py
@@ -13,22 +13,33 @@ class Reserva(models.Model):
     inidate = models.DateTimeField(null=False,default='')
     fimdate = models.DateTimeField(null=False,default='')
     type = models.CharField(max_length=20,null=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True,blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Estabelecimento(models.Model):
     name = models.CharField(max_length=200, null=False)
     releasedate = models.DateTimeField(null=False)
     type = models.CharField(max_length=20,null=False)
-    reserva = models.OneToOneField(Reserva, on_delete=models.CASCADE,null=True)
+    reserva = models.OneToOneField(Reserva, on_delete=models.CASCADE,null=True, blank=True)
 
+    def __str__(self):
+        return self.name
 
 class Estacionamento(models.Model):
     name = models.CharField(max_length=200, null=False)
     lugares = models.IntegerField(null=True)
+    EstEstabelecimento = models.OneToOneField(Estabelecimento, on_delete=models.CASCADE,null=True,blank=True)
 
+    def __str__(self):
+        return self.name
 
 class Praia(models.Model):
     name = models.CharField(max_length=100, null=False)
     NumeroEquipamentos = models.IntegerField(default='',null=True)
     reserva = models.OneToOneField(Reserva, on_delete=models.CASCADE,null=True, blank=True)
+
+    def __str__(self):
+        return self.name
