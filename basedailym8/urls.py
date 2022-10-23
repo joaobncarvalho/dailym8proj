3fdc0af14d6a1daeab57196f9cfbfb9a1288105c
João Carvalho <joaove>
2022-10-23 19:40:13 +0100

    push

--- basedailym8/urls.py
+++ basedailym8/urls.py
@@ -4,9 +4,14 @@ from . import views
 
 urlpatterns = [
     
-
+    #Paths API Reservas 
 
     path('reserva/',views.reservas, name="reserva"),
+    path('create-reserva/', views.createReservas, name="create-reserva"),
+    path('update-reserva/<str:pk>/', views.updateReservas, name="update-reserva"),
+
+    #FIM --- Paths API Reservas 
+
     path('', views.spot, name="spots"),
     path('spot/<str:pk>/', views.singlespot, name="singlespot"),
     path('estacionamento/', views.estacionamento, name="estacionamento"),
--- basedailym8/views.py
+++ basedailym8/views.py
@@ -1,8 +1,13 @@
 from multiprocessing import context
 from django.shortcuts import render, redirect
 from .models import Spot
+from .models import Reserva
+from .forms import ReservaForm
 
-   
+
+
+#SPOTS
+
 def spot(request):
     
     spots = Spot.objects.all()
@@ -12,11 +17,43 @@ def spot(request):
 def singlespot(request,pk):
     return render(request, 'basedailym8/singlespot.html')
 
+#FIM --- SPOTS
+
 def home(request):
     return render(request, 'main.html')
 
+#CRUD for reservas 
+
 def reservas(request):
     return render(request, 'reserva.html')
+
+def createReservas(request):
+    form = ReservaForm()
+
+    if request.method == 'POST':
+        form = ReservaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('reserva')
+
+    context = {'form':form}
+    return render(request, "basedailym8/reserva_form.html", context)
+
+def updateReservas(request,pk):
+    
+    reserva = Reserva.objects.get(id=pk)
+    form = ReservaForm(instance=reserva)
+
+    if request.method == 'POST':
+        form = ReservaForm(request.POST, instance=reserva)
+        if form.is_valid():
+            form.save()
+            return redirect('reserva')
+
+    context = {'form':form}
+    return render(request, "basedailym8/reserva_form.html", context)
+
+#FIM --- CRUD for reservas 
 
 def estacionamento(request):
     return render(request, 'estacionamento.html')
