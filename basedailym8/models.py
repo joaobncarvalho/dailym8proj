b9a77c9f3cbaf277deea063696277b2a025351c6
João Carvalho <joaove>
2022-11-03 16:47:53 +0000

    uyu

--- basedailym8/models.py
+++ basedailym8/models.py
@@ -9,6 +9,73 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Pratos(models.Model):
+    featured_image =models.ImageField(null=True, blank=True, default="logo.png")
+    name = models.CharField(max_length=200, null=False)
+    price = models.IntegerField(null=False, default='', blank=False)
+
+    def __str__(self):
+        return str(self.name)
+
+class Menu(models.Model):
+    
+    featured_image =models.ImageField(null=True, blank=True, default="logo.png")
+    name = models.CharField(max_length=200, null=False)
+    pratos = models.ForeignKey(Pratos, on_delete=models.CASCADE,null=True,blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+class Restaurante(models.Model):
+    RESTAURANTE_TYPE = (
+        ('Internacional','rest_internacional'),
+        ('Americano','rest_americano'),
+        ('Português','rest_portugues'),
+        ('Chinês','rest_chines'),
+        ('Italiano','rest_italiano'),
+    )
+    featured_image =models.ImageField(null=True, blank=True, default="logo.png")
+    name = models.CharField(max_length=200, null=False)
+    releasedate = models.DateTimeField(null=False)
+    type = models.CharField(max_length=20,choices=RESTAURANTE_TYPE,null=False)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE,null=True,blank=True)
+
+    
+
+    def __str__(self):
+        return str(self.name)
+
+
+
+class Estacionamento(models.Model):
+    featured_image =models.ImageField(null=True, blank=True, default="logo.png")
+    name = models.CharField(max_length=200, null=False)
+    releasedate = models.DateTimeField(null=False)
+    lugares = models.IntegerField(null=False, blank = False, default='')
+    
+    
+
+    def __str__(self):
+        return str(self.name)
+
+class Praiaequip(models.Model):
+    
+    EQUIPAMENTO_TYPE = (
+        ('Espreguiçadeira','equi_espreguicadeira'),
+        ('Toldo','equi_toldo'),
+    )
+    
+    featured_image =models.ImageField(null=True, blank=True, default="logo.png")
+    name = models.CharField(max_length=200, null=False)
+    releasedate = models.DateTimeField(null=False)
+    lugares = models.IntegerField(null=False, blank = False, default='')
+    restaurante = models.ForeignKey(Restaurante, on_delete=models.CASCADE,null=True,blank=True)
+    type = models.CharField(max_length=20,choices=EQUIPAMENTO_TYPE,null=False)
+
+    def __str__(self):
+        return str(self.name)
+
+
 class Spot(models.Model):
     SPOT_TYPE = (
         ('Restaurante','spot_restaurante'),
